1c5e7f6903de086c01d8d54602fe43c63f8af454
Hueckel <chlorine17.xenon54@gmail.com>
2025-01-22 00:36:47 +0900

    feat: show a plugin name on notification windows

--- lua/file-association/path.lua
+++ lua/file-association/path.lua
@@ -9,54 +9,54 @@ Path.__index = Path
 ---@param retFilepath table
 ---@return Path
 function Path:new(retFilepath)
-  local obj = setmetatable({}, self)
-  obj.name = Path:_retPath(vim.bo.filetype, retFilepath)
-  obj.ext = obj.name:match('%.([%w_]+)$')
-  obj.is_dir = vim.fn.isdirectory(obj.name) ~= 0
-  obj.is_file = vim.fn.filereadable(obj.name) ~= 0
-  return obj
+	local obj = setmetatable({}, self)
+	obj.name = Path:_retPath(vim.bo.filetype, retFilepath)
+	obj.ext = obj.name:match("%.([%w_]+)$")
+	obj.is_dir = vim.fn.isdirectory(obj.name) ~= 0
+	obj.is_file = vim.fn.filereadable(obj.name) ~= 0
+	return obj
 end
 
 function Path:copyToClipboard()
-  if self.is_dir or self.is_file then
-    local is_copy_success = vim.fn.setreg('+', self.name)
-    if is_copy_success == 0 then
-      vim.notify('Copied to clipboard')
-    end
-  else
-    vim.notify('File not found: ' .. self.name, 3)
-  end
+	if self.is_dir or self.is_file then
+		local is_copy_success = vim.fn.setreg("+", self.name)
+		if is_copy_success == 0 then
+			vim.notify("Copied to clipboard", vim.log.levels.INFO, { title = "file-association.nvim" })
+		end
+	else
+		vim.notify("File not found: " .. self.name, vim.log.legels.WARN, { title = "file-association.nvim" })
+	end
 end
 
 ---@param input string
 ---@return string
 function Path:_replaceEnvVar(input)
-  local function retDirOrRawstr(env_var)
-    return vim.env[env_var] or ('%' .. env_var .. '%')
-  end
-  local ret_value = string.gsub(input, '%%(.-)%%', retDirOrRawstr)
-  return ret_value
+	local function retDirOrRawstr(env_var)
+		return vim.env[env_var] or ("%" .. env_var .. "%")
+	end
+	local ret_value = string.gsub(input, "%%(.-)%%", retDirOrRawstr)
+	return ret_value
 end
 
 ---@param filetype string
 ---@param ret_filepath table
 ---@return string
 function Path:_retPath(filetype, ret_filepath)
-  local user_func = ret_filepath[filetype]
-  local path
+	local user_func = ret_filepath[filetype]
+	local path
 
-  if user_func then
-    path = user_func()
-  elseif filetype == 'netrw' then
-    local netrw_call = vim.fn['netrw#Call']
-    local entry = netrw_call('NetrwGetWord')
-    path = netrw_call('NetrwFile', entry)
-  else
-    local filepath_raw = vim.fn.expand('<cfile>')
-    path = Path:_replaceEnvVar(filepath_raw)
-  end
+	if user_func then
+		path = user_func()
+	elseif filetype == "netrw" then
+		local netrw_call = vim.fn["netrw#Call"]
+		local entry = netrw_call("NetrwGetWord")
+		path = netrw_call("NetrwFile", entry)
+	else
+		local filepath_raw = vim.fn.expand("<cfile>")
+		path = Path:_replaceEnvVar(filepath_raw)
+	end
 
-  return path
+	return path
 end
 
 return Path
