4990836afcdf869090706f24ba8bc7e22caeec46
Hueckel <chlorine17.xenon54@gmail.com>
2025-01-22 01:12:02 +0900

    fix: align with the original format

--- lua/file-association.lua
+++ lua/file-association.lua
@@ -2,9 +2,9 @@
 ---@field ret_filepath table Enter the name of the filer plug-in for the key and the function that returns file path for the value
 ---@field exts table Associate exts to a program
 local config = {
-	ret_filepath = {},
-	exts = {},
-	filer = '',
+  ret_filepath = {},
+  exts = {},
+  filer = '',
 }
 
 ---@class MyModule
@@ -22,42 +22,42 @@ local Path = require('file-association.path')
 
 ---@param args Config?
 M.setup = function(args)
-	M.config = vim.tbl_deep_extend('force', M.config, args or {})
-	local at = AssociationTable:new(M.config.exts)
-	M.association_table = at.table
-	M.default_filer = DefaultFiler:new(M.config.filer)
+  M.config = vim.tbl_deep_extend('force', M.config, args or {})
+  local at = AssociationTable:new(M.config.exts)
+  M.association_table = at.table
+  M.default_filer = DefaultFiler:new(M.config.filer)
 end
 
 M.open_with = function()
-	if not M.association_table then
-		vim.notify(
-			"Association table is not initialized. Please call require('file-association').setup first.",
-			vim.log.levels.ERROR,
-			{ title = 'file-association.nvim' }
-		)
-		return 1
-	else
-		local path = Path:new(M.config.ret_filepath)
+  if not M.association_table then
+    vim.notify(
+      "Association table is not initialized. Please call require('file-association').setup first.",
+      vim.log.levels.ERROR,
+      { title = 'file-association.nvim' }
+    )
+    return 1
+  else
+    local path = Path:new(M.config.ret_filepath)
 
-		if path.is_dir then
-			M.default_filer:openDir(path.name)
-		elseif not path.is_file then
-			vim.notify('File not found: ' .. path.name, vim.log.levels.WARN, { title = 'file-association.nvim' })
-		else
-			local program = M.association_table[path.ext]
-			if program then
-				vim.uv.spawn(program, { args = { path.name } })
-			else
-				vim.ui.open(path.name)
-			end
-		end
-		return 0
-	end
+    if path.is_dir then
+      M.default_filer:openDir(path.name)
+    elseif not path.is_file then
+      vim.notify('File not found: ' .. path.name, vim.log.levels.WARN, { title = 'file-association.nvim' })
+    else
+      local program = M.association_table[path.ext]
+      if program then
+        vim.uv.spawn(program, { args = { path.name } })
+      else
+        vim.ui.open(path.name)
+      end
+    end
+    return 0
+  end
 end
 
 M.copy_filepath = function()
-	local path = Path:new(M.config.ret_filepath)
-	path:copyToClipboard()
+  local path = Path:new(M.config.ret_filepath)
+  path:copyToClipboard()
 end
 
 return M
--- lua/file-association/default-filer.lua
+++ lua/file-association/default-filer.lua
@@ -7,62 +7,62 @@ DefaultFiler.__index = DefaultFiler
 ---@param userDefinedFiler string
 ---@return DefaultFiler
 function DefaultFiler:new(userDefinedFiler)
-	local obj = setmetatable({}, self)
-	obj.name = self:_retFilerName(userDefinedFiler, vim.uv.os_uname().sysname)
-	obj.errorlevel = self:_retErrorlevel(obj.name)
-	return obj
+  local obj = setmetatable({}, self)
+  obj.name = self:_retFilerName(userDefinedFiler, vim.uv.os_uname().sysname)
+  obj.errorlevel = self:_retErrorlevel(obj.name)
+  return obj
 end
 
 ---@param path string
 ---@return integer
 function DefaultFiler:openDir(path)
-	if self.errorlevel == 0 then
-		vim.uv.spawn(self.name, { args = { path } })
-	elseif self.errorlevel == 1 then
-		vim.notify('Filer is not executable: ' .. self.name, vim.log.levels.WARN, { title = 'file-association.nvim' })
-	else
-		vim.notify(
-			'Could not open default filer due to unsupported OS',
-			vim.log.levels.ERROR,
-			{ title = 'file-association.nvim' }
-		)
-	end
-	return self.errorlevel
+  if self.errorlevel == 0 then
+    vim.uv.spawn(self.name, { args = { path } })
+  elseif self.errorlevel == 1 then
+    vim.notify('Filer is not executable: ' .. self.name, vim.log.levels.WARN, { title='file-association.nvim' })
+  else
+    vim.notify(
+      'Could not open default filer due to unsupported OS',
+      vim.log.levels.ERROR,
+      { title='file-association.nvim' }
+    )
+  end
+  return self.errorlevel
 end
 
 ---@param userDefinedFiler string
 ---@param sysname string
 ---@return string
 function DefaultFiler:_retFilerName(userDefinedFiler, sysname)
-	local filer
-	if userDefinedFiler == '' then
-		if sysname == 'Darwin' then
-			filer = 'open'
-		elseif sysname == 'Windows_NT' then
-			filer = 'explorer'
-		elseif sysname == 'Linux' then
-			filer = 'xdg-open'
-		end
-	else
-		filer = userDefinedFiler
-	end
-	return filer
+  local filer
+  if userDefinedFiler == '' then
+    if sysname == 'Darwin' then
+      filer = 'open'
+    elseif sysname == 'Windows_NT' then
+      filer = 'explorer'
+    elseif sysname == 'Linux' then
+      filer = 'xdg-open'
+    end
+  else
+    filer = userDefinedFiler
+  end
+  return filer
 end
 
 ---@param filer string
 ---@return integer
 function DefaultFiler:_retErrorlevel(filer)
-	local errorlevel
-	if vim.fn.executable(filer) == 0 then
-		if filer == '' then
-			errorlevel = 2
-		else
-			errorlevel = 1
-		end
-	else
-		errorlevel = 0
-	end
-	return errorlevel
+  local errorlevel
+  if vim.fn.executable(filer) == 0 then
+    if filer == '' then
+      errorlevel = 2
+    else
+      errorlevel = 1
+    end
+  else
+    errorlevel = 0
+  end
+  return errorlevel
 end
 
 return DefaultFiler
